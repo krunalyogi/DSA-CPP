7d73158c3d689cec73132d11c3fb4b2fe3d36c58
krunal yogi <krunalyogi9@gmail.com>
2026-07-02 17:37:49 +0530

    counting digits

--- BasicMaths/countdigits.cpp
+++ BasicMaths/countdigits.cpp
@@ -1,12 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cin>>n;
+    int n=1234;
     int count = 0;
     while(n>0){
-        int temp= n/10;
-        n=temp;
+        n/=10;
         count++;
     }
     cout<<count;
