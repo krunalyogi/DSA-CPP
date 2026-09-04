baa54df98be84513af682b164d5b56d8d462069f
krunal yogi <krunalyogi9@gmail.com>
2026-09-04 19:09:48 +0530

    learning selection sort and implementation

---
+++ 03-Sorting/Sorting-I/selectionsort.cpp
@@ -0,0 +1,18 @@
+//we are implementing selection sort 
+#include<iostream>
+#include<algorithm>
+#include<array>
+using namespace std;
+int main(){
+    int arr[6] = {13,46,24,52,20,9};
+    for(int i=0;i<5;i++){
+        int min = i;
+        for(int j=i;j<=5;j++){
+            if(arr[j]<arr[min]){
+                min =j;
+            }
+            swap(arr[min],arr[i]);
+        }
+    }
+    for(int k=0;k<6;k++) cout<<arr[k]<<" ";
+}
